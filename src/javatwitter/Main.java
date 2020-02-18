91ef47857cbf78f5f0726251c9c8aea82a734870
danieldodi <danieldodi@users.noreply.github.com>
2020-02-18 19:56:09 +0000

    Optimizations
    Created new class "ConnectionAPI.java" to manage keys and tokens.
    
    Added comments and fixed some bugs.

--- src/javatwitter/Main.java
+++ src/javatwitter/Main.java
@@ -4,26 +4,19 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Scanner;
 import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
-import twitter4j.TwitterFactory;
-import twitter4j.conf.ConfigurationBuilder;
 
 /**
  *
  * @author Daniel
  */
 public class Main {
-
-    // Keys and tokens necessary to connect to Twitter API
-    private static final String CONSUMER_KEY = "HDZbUhcAZUehKDGpsTqmUzi2H";
-    private static final String CONSUMER_SECRET = "YVUZtoJ1gg3VQFRtFNZJgTvIc8afLnio7tnM84CMIEX7VcKvOb";
-    private static final String ACCESS_TOKEN = "1015941676974706688-R3PN3S5OhspZg4y7WwxoSZVFUiv2Zt";
-    private static final String ACCESS_TOKEN_SECRET = "Sr7mrzqS4kC05Sx8xrIPxFgtS7LqEufuWt6R3YKp9ZL29";
-    
     public static void main(String[] args) throws TwitterException, IOException {
         // Scanner for username input
         Scanner input = new Scanner(System.in);
@@ -31,62 +24,51 @@ public class Main {
         String user = input.nextLine();
         System.out.println("");
         
-        // Calls getTwitterInstance() method
-        getTwitterInstance();
-        Twitter twitter = getTwitterInstance();
+        // Calls getTwitterInstance() method and assigns it to a new Twitter object
+        Twitter twitter = ConnectionAPI.getTwitterInstance();
         
         // Calls getUserTimeline() method receiving the twitter instance and inputed username
         getUserTimeline(twitter, user);
     }
 
-    // Method to connect to Twitter instance
-    public static Twitter getTwitterInstance() {
-        ConfigurationBuilder cb = new ConfigurationBuilder();
-        cb.setDebugEnabled(true)
-            .setOAuthConsumerKey(CONSUMER_KEY)
-            .setOAuthConsumerSecret(CONSUMER_SECRET)
-            .setOAuthAccessToken(ACCESS_TOKEN)
-            .setOAuthAccessTokenSecret(ACCESS_TOKEN_SECRET);
-        TwitterFactory tf = new TwitterFactory(cb.build());
-        Twitter twitter = tf.getInstance();
-        
-        return twitter;
-    }
-    
     // Method to retrieve last 20 twits from an user
-    public static void getUserTimeline(Twitter twitter, String user) throws TwitterException, IOException {
+    private static void getUserTimeline(Twitter twitter, String user) throws TwitterException, IOException {
         List<Status> statuses = twitter.getUserTimeline(user);
         String userName = "@" + statuses.get(0).getUser().getScreenName();
         int twitNumber = 1;
         
+        // Get the current date and time and format it to desired layout
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+        LocalDateTime ldt = LocalDateTime.now();
+        
         System.out.println("Latest twits: ");
         
-        // Recorre all the twits and prints them
+        // Scan all the twits and prints them
         for (Status status : statuses) {
             String twits = "Twit #" + twitNumber++ + "\n\n" + status.getText() + "\n________________________________________________";
             System.out.println(twits + "\n");
-        }
+        } twitNumber = 1;
         
         // Option to save twits to a .txt file
         System.out.print("Do you want to save these twits? [Y/N]: ");
         Scanner input = new Scanner(System.in);
         String option = input.nextLine().toUpperCase();
         
+        // Save twits to a file if user inputs yes
         if (option.equals("Y")) {
             FileWriter fileWriter = new FileWriter(new File("files" + File.separator + userName + "_twits.txt"));
-            BufferedWriter writer = new BufferedWriter(fileWriter);
-            
-            
-            writer.write("Twits from user "  + userName + "\n\n");
-            
-            for (Status status : statuses) {
-                String twits = "Twit #" + twitNumber++ + "\n" + status.getText();
-                writer.write(twits + "\n________________________________________________");
-                writer.newLine();
-                writer.newLine();
+            try (BufferedWriter writer = new BufferedWriter(fileWriter)) {
+                // Write current date and time
+                writer.write("Twits from user "  + userName + " at " + dtf.format(ldt) +"\n\n");
+                
+                // Scan all the twits and writes them to the file
+                for (Status status : statuses) {
+                    String twits = "Twit #" + twitNumber++ + "\n" + status.getText();
+                    writer.write(twits + "\n________________________________________________");
+                    writer.newLine();
+                    writer.newLine();
+                }
             }
-            
-            writer.close();
         }
     }
 }
